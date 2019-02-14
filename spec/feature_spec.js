d5d5af0654c2a4a00b1e4f913e60475191e99b64
Simon Cass <scass1991@gmail.com>
2019-02-14 12:23:08 +0000

    All feature tests added for non special items

--- spec/feature_spec.js
+++ spec/feature_spec.js
@@ -19,5 +19,26 @@ describe("Gilded Rose", function() {
         expect(items[0].quality).toEqual(0);
       })
     })
+
+    describe("Sell by date", function() {
+      it("reduces by 1 when the shop is updated", function() {
+        const gildedRose = new Shop([ new Item("blah", 10, 10) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].sellIn).toEqual(9);
+      })
+      it("can be negative to represent an item being past its sell by date", function() {
+        const gildedRose = new Shop([ new Item("blah", 0, 10) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].sellIn).toEqual(-1);
+      })
+    })
+
+    describe("Name", function() {
+      it("Never changes", function() {
+        const gildedRose = new Shop([ new Item("blah", 10, 10) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].name).toEqual("blah");
+      })
+    })
   })
 })
