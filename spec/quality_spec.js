5a3574b8e09e8c6193675c40e4ae6ed74c1009ae
Simon Cass <scass1991@gmail.com>
2019-02-14 17:06:05 +0000

    Added quality file, realised functions cant be changed when stored as const, changed to var

--- spec/quality_spec.js
+++ spec/quality_spec.js
@@ -1,26 +1,26 @@
 describe("Quality changes", function() {
 
   it("quality should should reduce", function() {
-    const quality = 10
-    const qual_change = -1
-    expect(Quality.changeQual(quality, qual_change)).toEqual(9);
+    var quality = 10
+    var qual_change = -1
+    expect(Quality.changeQual(quality,qual_change)).toEqual(9);
   });
 
   it("quality should increase", function() {
-    const quality = 10
-    const qual_change = 1
-    expect(Quality.changeQual(quality, qual_change)).toEqual(11);
+    var quality = 10
+    var qual_change = 1
+    expect(Quality.changeQual(quality,qual_change)).toEqual(11);
   })
 
   it("quality should not go below 0", function() {
-    const quality = 0
-    const qual_change = -1
-    expect(Quality.changeQual(quality, qual_change)).toEqual(0);
+    var quality = 0
+    var qual_change = -1
+    expect(Quality.changeQual(quality,qual_change)).toEqual(0);
   })
 
   it("quality should not go above 50", function() {
-    const quality = 50
-    const qual_change = +1
-    expect(Quality.changeQual(quality, qual_change)).toEqual(0);
+    var quality = 50
+    var qual_change = +1
+    expect(Quality.changeQual(quality,qual_change)).toEqual(50);
   });
 });
---
+++ src/quality.js
@@ -0,0 +1,27 @@
+(function(exports){
+
+  function changeQual(quality,qual_change){
+    quality += qual_change
+    quality = qualityMaximum(quality)
+    quality = qualityMinimum(quality)
+    return quality
+  }
+
+  function qualityMaximum(quality){
+    if (quality > 50) {
+      quality = 50;
+    };
+    return quality
+  };
+
+  function qualityMinimum(quality){
+    if (quality < 0) {
+      quality = 0;
+    }
+    return quality
+  };
+
+  exports.Quality = {
+    changeQual: changeQual
+  };
+})(this);
