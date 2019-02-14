605a69a13885f26b86d08a7fa7706b85b98b70db
Simon Cass <scass1991@gmail.com>
2019-02-14 14:33:05 +0000

    Sulfuras tests added & passing

---
+++ src/sulfuras.js
@@ -0,0 +1,15 @@
+//https://developer.mozilla.org/en-US/docs/web/javascript/reference/statements/export
+
+(function(exports){
+  function Sulfuras(sellIn, quality){
+    this.sellIn = sellIn
+    this.quality = quality
+    }
+    Sulfuras.prototype.updateSellIn = function () {
+    };
+
+    Sulfuras.prototype.updateQuality = function () {
+    };
+
+    exports.Sulfuras = Sulfuras;
+  })(this);
