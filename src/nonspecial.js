4b24124111c1935a860979794da965be893c135f
Simon Cass <scass1991@gmail.com>
2019-02-14 17:11:01 +0000

    Refactor non special items file

--- src/nonspecial.js
+++ src/nonspecial.js
@@ -10,10 +10,10 @@
     NonSpecial.prototype.updateQuality = function () {
       if (this.quality === 0) {
         this.quality
-      } else if (this.sellIn >= 1)  {
-        this.quality -= 1
+      } else if (this.sellIn > 0)  {
+        this.quality = Quality.changeQual(this.quality, -1)
       } else {
-        this.quality -= 2
+        this.quality = Quality.changeQual(this.quality, -2)
       }
     };
 
