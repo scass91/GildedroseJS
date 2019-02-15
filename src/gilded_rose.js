42180d31c456a72ad0572136ac5327029651a232
Simon Cass <scass1991@gmail.com>
2019-02-15 15:33:15 +0000

    refactor of gilded_rose.js

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -12,18 +12,21 @@ class Shop {
 	}
 
 	change() {
+
+    var option = {
+      "Sulfuras, Hand of Ragnaros" : Sulfuras,
+      "Aged Brie" : AgedBrie,
+      "Backstage passes to a TAFKAL80ETC concert" : Backstage,
+      "Conjured" : Conjured
+    }
+
 		for (var i = 0; i < this.items.length; i++) {
-			if (this.items[i].name == "Sulfuras, Hand of Ragnaros") {
-				var item = new Sulfuras(this.items[i].sellIn, this.items[i].quality)
-			} else if (this.items[i].name == "Aged Brie") {
-				var item = new AgedBrie(this.items[i].sellIn, this.items[i].quality)
-			} else if (this.items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-				var item = new Backstage(this.items[i].sellIn, this.items[i].quality)
-			} else if (this.items[i].name == "Conjured") {
-				var item = new Conjured(this.items[i].sellIn, this.items[i].quality)
-			} else {
-				var item = new NonSpecial(this.items[i].sellIn, this.items[i].quality)
-			}
+      if (!(this.items[i].name in option)) {
+        var item = new NonSpecial(this.items[i].sellIn, this.items[i].quality)
+      } else {
+        var item = new option[this.items[i].name](this.items[i].sellIn, this.items[i].quality)
+      }
+
 			item.updateQuality()
 			item.updateSellIn()
 			this.items[i].sellIn = item.sellIn
@@ -31,5 +34,5 @@ class Shop {
 
 			return this.items
 		}
-	}
+  }
 }
